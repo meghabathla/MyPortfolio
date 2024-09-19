70489bcfd05a558792d91ea3555ea1fe003dc8a7
Megha-Bathla <bathlamegha6@gmail.com>
2024-09-19 22:39:49 +0530

    change transition time

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
-          transition={{ duarion: 1 }}
+          transition={{ duarion: 1.3 }}
           className="w-full lg:w-1/2 lg:p-8"
         >
           <div className="flex items-center justify-center">
@@ -33,7 +33,7 @@ const About = () => {
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 1 }}
           className="w-full lg:w-1/2"
         >
           <div className="flex justify-center lg:justify-start">
